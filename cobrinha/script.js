d0cdd0e0ad3ef843498356dddc5580f4ff8ef5cc
Carlos Estevao <carlosestevaobs@gmail.com>
2020-10-30 15:08:14 -0300

    finalizacao

--- cobrinha/script.js
+++ cobrinha/script.js
@@ -10,6 +10,11 @@ snake[0] = {
 }
 
 let direction = "right";
+let food = {
+    x: Math.floor(Math.random() * 15 + 1) * box,
+    y: Math.floor(Math.random() * 15 + 1) * box
+
+}
 
 /* função que desenha o canbas */
 function criarBG() {
@@ -25,9 +30,38 @@ function criarCobrinha() {
     }
 }
 
+
+function drawFood() {
+    context.fillStyle = "red";
+    context.fillRect(food.x, food.y, box, box);
+
+}
+
+document.addEventListener('keydown', update);
+
+function update(event) {
+    if (event.keyCode == 37 && direction != "right") direction = "left";
+    if (event.keyCode == 38 && direction != "down") direction = "up";
+    if (event.keyCode == 39 && direction != "left") direction = "right";
+    if (event.keyCode == 40 && direction != "up") direction = "down";
+}
+
 function iniciarJogo() {
+    if (snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
+    if (snake[0].x < 0 && direction == "left") snake[0].x = 16 * box;
+    if (snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
+    if (snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;
+
+    for (i = 1; i < snake.length; i++) {
+        if (snake[0].x == snake[i].x && snake[0].y == snake[i].y) {
+            clearInterval(jogo);
+            alert("Game over!");
+        }
+    }
+
     criarBG();
     criarCobrinha();
+    drawFood();
 
     /* ponto de partida */
     let snakeX = snake[0].x;
@@ -48,12 +82,19 @@ function iniciarJogo() {
     if (direction == 'down') {
         snakeY += box;
     }
-    /* retirar último elemento do array */
-    snake.pop();
+
+    if (snakeX != food.x || snakeY != food.y) {
+        /* retirar último elemento do array */
+        snake.pop();
+    } else {
+        food.x = Math.floor(Math.random() * 15 + 1) * box;
+        food.y = Math.floor(Math.random() * 15 + 1) * box;
+    }
+
 
     let newHead = {
         x: snakeX,
-        y: snakey
+        y: snakeY
     }
     snake.unshift(newHead);
 
